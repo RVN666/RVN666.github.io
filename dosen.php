cff3a8f474f44c5c629ca852559087c5fae11314
RVN666 <97417613+RVN666@users.noreply.github.com>
2022-01-10 13:37:01 +0700

    Add files via upload

---
+++ dosen.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+  <html>
+  <head>
+  <form action="koneksi2.php" method="get">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="style.css" media="screen" title="no title">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+      <div class="login">
+
+          <div class="avatar">
+            <i class="fa fa-user"></i>
+          </div>
+
+          <h2>Login Dosen</h2>
+
+          <div class="box-login">
+            <i class="fas fa-envelope-open-text"></i>
+            <input type="text" placeholder="Nis">
+          </div>
+
+          <div class="box-login">
+            <i class="fas fa-lock"></i>
+            <input type="text" placeholder="Password">
+          </div>
+
+          <button type="submit" name="login" class="btn-login">Login</button>
+          <div class="bottom">
+            <a href="#">Register</a>
+            <a href="#">Forgot Password</a>
+          </div>
+      </div>
+  </head>
+  </html>
